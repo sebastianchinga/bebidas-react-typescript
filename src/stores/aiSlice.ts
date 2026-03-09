82da650727b7bbecac0942455175ade60bea17fb
Sebastian <jchingapalacios05@gmail.com>
2026-03-09 06:52:18 -0500

    feat: creando el slice de IA

---
+++ src/stores/aiSlice.ts
@@ -0,0 +1,9 @@
+import type { StateCreator } from "zustand";
+
+export type AISlice = {
+    recipe: string
+}
+
+export const createAISlice: StateCreator<AISlice, [], [], AISlice> = () => ({
+    recipe: ''
+})
--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,14 +3,16 @@ import { devtools } from 'zustand/middleware';
 import { createRecipesSlice, type RecipesSliceType } from './recipeSlice';
 import { createFavoritesSlice, type FavoritesSliceType } from './favoritesSlice'
 import { createNotificationSlice, type NotificationSliceType } from './notificationSlice';
+import { createAISlice, type AISlice } from './aiSlice';
 
 // RecipesSliceType: Es el type del slice
 // ...a: Trae como argumentos los métodos get, set y api que serán obviamente una copia
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType & AISlice>()(
     devtools((...a) => ({
         // Retorna una copia del slice creado donde le mandaremos los tres métodos con ...a
         ...createRecipesSlice(...a),
         ...createFavoritesSlice(...a),
-        ...createNotificationSlice(...a)
+        ...createNotificationSlice(...a),
+        ...createAISlice(...a)
     }))
 )
