be8cd5bec718d5143f056eede81ff0a192ca41f9
Sebastian <jchingapalacios05@gmail.com>
2026-03-13 07:03:51 -0500

    feat: modificando router

--- src/router.tsx
+++ src/router.tsx
@@ -2,9 +2,9 @@ import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import IndexPage from './views/IndexPage'
 import Layout from './layouts/Layout'
-import GenerateAI from './views/GenerateAI'
 
 const FavoritesPage = lazy(() => import('./views/FavoritesPage'))
+const GenerateAI = lazy(() => import('./views/GenerateAI'))
 
 export default function AppRouter() {
     return (
@@ -17,7 +17,11 @@ export default function AppRouter() {
                             <FavoritesPage />
                         </Suspense>
                     } />
-                    <Route path='/generate' element={<GenerateAI />} index />
+                    <Route path='/generate' element={
+                        <Suspense fallback='Cargando...'>
+                            <GenerateAI/>
+                        </Suspense>
+                    } />
                 </Route>
             </Routes>
         </BrowserRouter>
